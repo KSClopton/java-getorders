1180ecbf913d8e7929811f89770165bc55b70c83
KSClopton <kyle.clopton@outlook.com>
2020-08-12 21:38:07 -0400

    Added the first POST

--- orders/src/main/java/com/example/orders/controllers/CustomerController.java
+++ orders/src/main/java/com/example/orders/controllers/CustomerController.java
@@ -4,13 +4,14 @@ import com.example.orders.OrderCounts;
 import com.example.orders.models.Customer;
 import com.example.orders.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 
@@ -47,4 +48,35 @@ public class CustomerController {
         List<OrderCounts> myList = customerServices.getOrderCount();
         return new ResponseEntity<>(myList, HttpStatus.OK);
     }
+
+    // http://localhost:2019/customers/customer - Adds a new customer and orders
+    // POST Request Body -> New Customer =>
+    @PostMapping(value = "/customer", consumes = "applications/json")
+    public ResponseEntity<?> addNewCustomer(@Valid @RequestBody Customer newCustomer){
+        newCustomer.setCustcode(0);
+        newCustomer = customerServices.save(newCustomer);
+
+        // return location of new customer
+        // location => http://localhost:2019/customers/customer/id
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newCustomerURI = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/" + newCustomer.getCustcode())
+                .build()
+                .toUri();
+        responseHeaders.setLocation(newCustomerURI);
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+    // http://localhost:2019/customers/customer/{custcode} - completely replaces the customer record including associated orders with the provided data
+    // PUT Request Body -> All the information
+    @PutMapping(value = "/customer/{custcode}", consumes = "application/json")
+    public ResponseEntity<?> updateFullCustomer(@Valid @RequestBody Customer updateCustomer, @PathVariable long custcode){
+        
+    }
+
+    // http://localhost:2019/customers/customer/{custcode} - updates customers with the new data.
+
+    // http://localhost:2019/customers/customer/{custcode} - Deletes the given customer including any associated orders
+
+
 }
--- orders/src/main/java/com/example/orders/services/CustomerServiceImp.java
+++ orders/src/main/java/com/example/orders/services/CustomerServiceImp.java
@@ -2,21 +2,52 @@ package com.example.orders.services;
 
 import com.example.orders.OrderCounts;
 import com.example.orders.models.Customer;
+import com.example.orders.models.Order;
 import com.example.orders.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Transactional
 @Service(value = "customerServices")
 public class CustomerServiceImp implements CustomerService {
     @Autowired
     CustomerRepository custrepos;
 
+    @Transactional
     @Override
     public Customer save(Customer customer) {
+        Customer newCustomer = new Customer();
+        newCustomer.setCustname(customer.getCustname());
+        newCustomer.setCustcity(customer.getCustcity());
+        newCustomer.setWorkingarea(customer.getWorkingarea());
+        newCustomer.setCustcountry(customer.getCustcountry());
+        newCustomer.setGrade(customer.getGrade());
+        newCustomer.setOpeningamt(customer.getOpeningamt());
+        newCustomer.setReceiveamt(customer.getReceiveamt());
+        newCustomer.setPaymentamt(customer.getPaymentamt());
+        newCustomer.setOutstandingamt(customer.getOutstandingamt());
+        newCustomer.setPhone(customer.getPhone());
+
+        // many to many
+        // payment must already exist
+
+
+        // one to many
+        newCustomer.getOrders().clear();
+        for(Order o : customer.getOrders()){
+            Order newOrder = new Order();
+            newOrder.setOrdamount(o.getOrdamount());
+            newOrder.setAdvanceamount(o.getAdvanceamount());
+            newOrder.setOrderdescription(o.getOrderdescription());
+            newOrder.setCustomer(newCustomer);
+
+            newCustomer.getOrders().add(newOrder);
+        }
         return custrepos.save(customer);
     }
 
@@ -41,7 +72,6 @@ public class CustomerServiceImp implements CustomerService {
     @Override
     public List<OrderCounts> getOrderCount() {
         List<OrderCounts> list = custrepos.findOrderCounts();
-//        return null;
         return list;
     }
 }
